0d0e6064646652272a8e99736f704d53c5593084
JohnLahoodFiveSides <1006081@student.sae.edu.au>
2016-08-05 11:48:19 +1000

    Level 3 of the game

---
+++ Assets/My Scripts/BasicEnemyMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicEnemyMove : MonoBehaviour {
+
+	private int speed = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.localPosition += transform.right * speed * Time.deltaTime; // or transform.position, both would work
+	}
+}

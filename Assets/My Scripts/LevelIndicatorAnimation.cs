231464ac168bd188e49a8f7ea5408e31331686cf
TristanIbanezFiveSides <1005132@student.sae.edu.au>
2016-08-06 20:04:12 +1000

    Added level intro blur + level name intro

---
+++ Assets/My Scripts/LevelIndicatorAnimation.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+public class LevelIndicatorAnimation : MonoBehaviour {
+
+    private float timer;
+    private float blurTime;
+
+    private GameObject playerMovement;
+    Player player;
+
+    DepthOfField blurEffect;
+
+	// Use this for initialization
+	void Start ()
+    {
+        blurTime = 0.4f;
+
+        blurEffect = GetComponent<DepthOfField>();
+
+        playerMovement = GameObject.FindGameObjectWithTag("Player");
+        player = playerMovement.GetComponent<Player>();
+
+        player.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > 4f)
+        {
+            blurEffect.aperture -= blurTime * Time.deltaTime;   
+        }
+
+
+        if (timer > 5f)
+        {
+            player.enabled = true;
+        }
+    }
+}

7595d74ff22e33f9f6663a93c017a9766258578c
JohnLahoodFiveSides <1006081@student.sae.edu.au>
2016-08-12 11:18:50 +1000

    Level 3 finished, Level 8 started

---
+++ Assets/My Scripts/TwoTargetMover.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TwoTargetMover : MonoBehaviour {
+
+	public Transform end0;
+	public Transform end1;
+	private int enemymover = 10;
+
+	public float speed = 1.0f;
+
+	private Transform target;
+
+	void Start()
+	{
+		target = end0;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		
+
+		for (int i = 0; i <= enemymover; i++) 
+		{
+			float distance = Vector3.Distance (transform.position, target.position);
+
+			if (distance < 0.5f) 
+			{
+				target = end1;
+			}
+
+			transform.position = Vector3.Lerp (transform.position, target.position, speed * Time.deltaTime);
+
+
+		}
+		
+	}
+}
